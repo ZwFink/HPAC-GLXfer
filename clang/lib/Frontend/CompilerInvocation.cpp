98888d2db46632e95a8f3778ab3280ef88c63c01
Iain Sandoe <iain@sandoe.co.uk>
2020-09-20 09:29:14 +0100

    [C++20][Modules][HU 1/5] Introduce header units as a module type.
    This is the first in a series of patches that introduce C++20 importable
    header units.
    
    These differ from clang header modules in that:
     (a) they are identifiable by an internal name
     (b) they represent the top level source for a single header - although
         that might include or import other headers.
    
    We name importable header units with the path by which they are specified
    (although that need not be the absolute path for the file).
    
    So "foo/bar.h" would have a name "foo/bar.h".  Header units are made a
    separate module type so that we can deal with diagnosing places where they
    are permitted but a named module is not.
    
    Differential Revision: https://reviews.llvm.org/D121095

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -2445,6 +2445,7 @@ static const auto &getFrontendActionTable() {
       {frontend::GenerateModule, OPT_emit_module},
       {frontend::GenerateModuleInterface, OPT_emit_module_interface},
       {frontend::GenerateHeaderModule, OPT_emit_header_module},
+      {frontend::GenerateHeaderUnit, OPT_emit_header_unit},
       {frontend::GeneratePCH, OPT_emit_pch},
       {frontend::GenerateInterfaceStubs, OPT_emit_interface_stubs},
       {frontend::InitOnly, OPT_init_only},
@@ -2461,7 +2462,7 @@ static const auto &getFrontendActionTable() {
       {frontend::MigrateSource, OPT_migrate},
       {frontend::RunPreprocessorOnly, OPT_Eonly},
       {frontend::PrintDependencyDirectivesSourceMinimizerOutput,
-          OPT_print_dependency_directives_minimized_source},
+       OPT_print_dependency_directives_minimized_source},
   };
 
   return Table;
@@ -4189,6 +4190,7 @@ static bool isStrictlyPreprocessorAction(frontend::ActionKind Action) {
   case frontend::GenerateModule:
   case frontend::GenerateModuleInterface:
   case frontend::GenerateHeaderModule:
+  case frontend::GenerateHeaderUnit:
   case frontend::GeneratePCH:
   case frontend::GenerateInterfaceStubs:
   case frontend::ParseSyntaxOnly:

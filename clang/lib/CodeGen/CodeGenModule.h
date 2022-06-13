505b9562b6a42df68ba5f0372ddf72d71d94791f
Mitch Phillips <31459023+hctim@users.noreply.github.com>
2022-06-13 09:49:07 -0700

    Revert "Also move WeakRefReferences in CodeGenModule::moveLazyEmssionStates"
    This reverts commit 0ecbedc0986bd4b7b90a60a5f31d32337160d4c4.
    
    Parent change broke the ASan buildbot. See
    https://reviews.llvm.org/D126781 for more information.

--- clang/lib/CodeGen/CodeGenModule.h
+++ clang/lib/CodeGen/CodeGenModule.h
@@ -1508,8 +1508,6 @@ public:
 
     assert(WeakRefReferences.empty() &&
            "Not all WeakRefRefs have been applied");
-    NewBuilder->WeakRefReferences = std::move(WeakRefReferences);
-
     NewBuilder->TBAA = std::move(TBAA);
   }
 

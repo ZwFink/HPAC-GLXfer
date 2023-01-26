f85de1a1654e986f86ffa43c5d39e6fd073f38a0
Zane Fink <finkzane@gmail.com>
2023-01-26 08:42:08 -0800

    find gpu architecture and compute capability

---
+++ approx/approx_utilities/detect_arch.py
@@ -0,0 +1,26 @@
+import sh
+import sys
+from pathlib import Path
+
+prefix = Path(sys.argv[1])
+binpath = prefix / Path('bin')
+
+amdarch_path = binpath / Path('amdgpu-arch')
+
+amdarch = sh.Command(amdarch_path)
+
+arch_amd = amdarch().split()
+
+if arch_amd:
+  print(f'amdgcn;{arch_amd[0]}')
+  sys.exit(0)
+
+else:
+  nvptxarch_path = binpath / Path('nvptx-arch')
+  nvptxarch = sh.Command(nvptxarch_path)
+  arch_nvptx = nvptxarch().split()
+  if arch_nvptx:
+      print(f'nvptx64;{arch_nvptx[0]}')
+      sys.exit(0)
+
+sys.exit(1)
